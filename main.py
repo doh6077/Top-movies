9c1240b2eb4aeeb7f817c13bf1ed76f2d76aa21f
doh6077 <kimdohee@sheridancollege.ca>
2024-07-08 15:25:50 -0400

    delete duplicate

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('FLASK_KEY')
-app.secret_key= os.environ.get('SECRET_KEY')
+#app.secret_key= os.environ.get('SECRET_KEY')
 bootstrap=Bootstrap(app)
 
 # SQLAlchemy configuration
