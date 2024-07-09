bcee46605ed8d013df107144c942639972919912
doh6077 <kimdohee@sheridancollege.ca>
2024-07-09 11:42:38 -0400

    fix add route issue

--- main.py
+++ main.py
@@ -9,14 +9,17 @@ import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('FLASK_KEY')
+#app.config['SECRET_KEY'] = "8BYkEfBA6O6donzWlSihBXox7C0sKR6b"
 bootstrap=Bootstrap(app)
 
 # SQLAlchemy configuration
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URI", "sqlite:///move-collection.db")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URI", "sqlite:///movies.db")
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///movies.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 MOVIE_DB_API_KEY = os.environ.get('MOVIE_DB_API_KEY')
+#MOVIE_DB_API_KEY = "2b2e1c217f8f977fdf867a6916ab3461"
 MOVIE_DB_SEARCH_URL = "https://api.themoviedb.org/3/search/movie"
 MOVIE_DB_INFO_URL = "https://api.themoviedb.org/3/movie"
 MOVIE_DB_IMAGE_URL = "https://image.tmdb.org/t/p/w500"
